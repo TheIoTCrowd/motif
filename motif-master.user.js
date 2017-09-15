ceccc2ac71fea5db11a9ca52b4694d25e8b6dc4c
TheIoTCrowd <theiotcrowd@tuta.io>
2017-09-15 19:37:09 +0100

    Update motif-master.user.js

--- motif-master.user.js
+++ motif-master.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Moderator Tools Improved for Stack Exchange
 // @namespace    https://raw.githubusercontent.com/TheIoTCrowd/motif/master/
-// @version      0.1.1
+// @version      0.1.2
 // @author       Aurora0001
 // @match        https://*.stackexchange.com/questions/*
 // @match        https://*.meta.stackexchange.com/questions/*
-// @require      motif-post.js?v=0.1.1
+// @require      motif-post-0.1.2.user.js
 // @grant        none
 // ==/UserScript==
 
