fdcae23d68f4dd852e1496cf31849e8054715773
TheIoTCrowd <theiotcrowd@tuta.io>
2017-09-15 16:11:07 +0100

    Create motif-master.js

---
+++ motif-master.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Moderator Tools Improved for Stack Exchange
+// @namespace    https://raw.githubusercontent.com/TheIoTCrowd/motif/master/
+// @version      0.1.0
+// @author       Aurora0001
+// @match        https://*.stackexchange.com/questions/*
+// @match        https://*.meta.stackexchange.com/questions/*
+// @require      motif-post.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // This page is intentionally left blank.
+})();
